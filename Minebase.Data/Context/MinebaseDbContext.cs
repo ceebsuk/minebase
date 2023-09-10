3722da74e143ac51c72ee4a9b57fa644e7c4c64e
Chris <chris@ceebs.co.uk>
2023-09-10 07:15:15 +0100

    Added initial db context

---
+++ Minebase.Data/Context/MinebaseDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Minebase.Data.Context;
+
+public class MinebaseDbContext : DbContext
+{
+    public MinebaseDbContext(DbContextOptions<MinebaseDbContext> options) : base(options)
+    {
+    }
+}
