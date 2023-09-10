4fc767cc664f3809e15ed73ba9d3b3fe55298dca
Chris <chris@ceebs.co.uk>
2023-09-10 07:16:26 +0100

    Added both the items and recipes db set

--- Minebase.Data/Context/MinebaseDbContext.cs
+++ Minebase.Data/Context/MinebaseDbContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Minebase.Domain.Models;
 
 namespace Minebase.Data.Context;
 
 public class MinebaseDbContext : DbContext
 {
+    public DbSet<Item> Items { get; set; }
+    public DbSet<Recipe> Recipes { get; set; }
+    
     public MinebaseDbContext(DbContextOptions<MinebaseDbContext> options) : base(options)
     {
     }
