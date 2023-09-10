241156ab9d73b466942455731389a45ffadbbdc0
Chris <chris@ceebs.co.uk>
2023-09-10 09:12:37 +0100

    Added comments to MinebaseDbContext.cs

--- Minebase.Data/Context/MinebaseDbContext.cs
+++ Minebase.Data/Context/MinebaseDbContext.cs
@@ -3,9 +3,19 @@ using Minebase.Domain.Models;
 
 namespace Minebase.Data.Context;
 
+/// <summary>
+/// A class representing the database context for the Minebase application.
+/// </summary>
 public class MinebaseDbContext : DbContext
 {
+    /// <summary>
+    /// The collection of items in the database.
+    /// </summary>
     public DbSet<Item> Items { get; set; }
+    
+    /// <summary>
+    /// The collection of recipes in the database.
+    /// </summary>
     public DbSet<Recipe> Recipes { get; set; }
     
     public MinebaseDbContext(DbContextOptions<MinebaseDbContext> options) : base(options)
